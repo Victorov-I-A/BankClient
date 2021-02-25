7cb9070236a53ebffaa1b0691a3ad8e452e085bf
ILYA VIKTOROV <ilia.victorov@gmail.com>
2021-02-25 22:57:06 +0300

    add many accounts, card number and pin code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 fun main() {
-    BankClient().startClient(9999, "put 500")
-    BankClient().startClient(9999, "put 500")
-    BankClient().startClient(9999, "put 800")
-    BankClient().startClient(9999, "get 1700")
+    BankClient().startClient(9999, "8888-8888-8888-8888 1234 put 1000")
+    BankClient().startClient(9999, "7777-7777-7777-7777 1234 get 1000")
+    BankClient().startClient(9999, "6666-6666-6666-6666 1234 check")
 }
